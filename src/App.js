47c3067b69d9c0764ba2ff53762c921975ef9cd9
himoura32 <shawn.ahearn@outlook.com>
2022-05-28 16:18:51 -0400

    coded callbacks/condRendering/styled vidDetail

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import youtube from './apis/youtube';
 import VideoList from './components/VideoList';
 import SearchBar from './components/SearchBar';
+import VideoDetail from './components/VideoDetail';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('super cars');
+  }
 
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -13,14 +18,35 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({
+      selectedVideo: video,
+    });
   };
 
   render() {
     return (
       <div className='ui container'>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
